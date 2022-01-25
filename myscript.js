98b4b7a28cd297f555f35656e2a30be053f1ccb0
Sara-Barbi <barbisara02@gmail.com>
2022-01-25 17:31:22 +0100

    10 email

--- myscript.js
+++ myscript.js
@@ -2,28 +2,41 @@
 
 const app = new Vue({
     el: "#root",
-    array
     
     data :{
-        num:null,
+        arrayEmail:[],
     },
     
 
     methods: {
-        email: function () {
+        carica: function () {
           axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((risposta) => {
 
               console.log(risposta.data.response);
+              this.arrayEmail.push(risposta.data.response);
+             
 
-              
+             
             })
             .catch((error) => {
               console.log(error);
             });
         },
       },
+      mounted() {
+        for(let i=0; i<10; i++){
+            this.carica()
+        }
+        
+        
+        
+        
+        
+      }
+
+    
     
     
     
